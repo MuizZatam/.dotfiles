d2f0e419586774eecbc918a6853b7be7b59fd777
MuizZatam <muizzatam110@gmail.com>
2024-10-19 12:10:41 -0400

    Fix touchpad behaviour

--- setup.sh
+++ setup.sh
@@ -1,17 +1,7 @@
 # Update
 sudo dnf up
 
-# Defining touchpad behaviour
-
-sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
- Section "InputClass"
-         Identifier "touchpad"
-         MatchIsTouchpad "on"
-         Driver "libinput"
-         Option "Tapping" "on"
- EndSection
-EOF
-
+# Defining touch behaviour
 sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
   Section "InputClass"
         Identifier "libinput touchpad catchall"
@@ -28,7 +18,34 @@ sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-t
         Driver "libinput"
         Option "NaturalScrolling" "True"
   EndSection
+
+EOF
+
+sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+ Section "InputClass"
+         Identifier "touchpad"
+         MatchIsTouchpad "on"
+         Driver "libinput"
+         Option "Tapping" "on"
+ EndSection
+
+ Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+        Option "NaturalScrolling" "True"
+ EndSection
+
+ Section "InputClass"
+        Identifier "libinput pointer catchall"
+        MatchIsPointer "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+        Option "NaturalScrolling" "True"
+ EndSection
+
 EOF
 
 # Installations
-sudo dnf install -y alacritty polybar picom neovim git curl nitrogen
+sudo dnf install -y alacritty polybar picom neovim curl nitrogen
